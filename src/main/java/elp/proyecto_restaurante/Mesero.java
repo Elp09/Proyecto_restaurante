c39116edba21d1cbb11251da25fe8408dcab245b
Elp09 <159044985+Elp09@users.noreply.github.com>
2024-03-25 09:51:38 -0600

    Correccion mesero
    se agrego generador de numero de mesas

--- src/main/java/elp/proyecto_restaurante/Mesero.java
+++ src/main/java/elp/proyecto_restaurante/Mesero.java
@@ -6,84 +6,78 @@ import javax.swing.JOptionPane;
 
 public class Mesero {
     
-    //Mesero 1
-        int id_Mesero001 = 10020;
-        String name_Mesero001 = "Julian Coto";
-        int mesas_Mesero001 = 0;
-        String turno_Mesero001 = "";
-        int propinas_Mesero001 = 0;
-        int rating_Mesero001 = 0;
+    private int id;
+    private String nombre;
+    private int mesas_asignadas[];
+    private Turno_Mesero turno;
+    private double propinas;
+    private double rating;
 
-//Mesero 2
-        int id_Mesero002 = 10025;
-        String name_Mesero002 = "Jose Cruz";
-        int mesas_Mesero002 = 0;
-        String turno_Mesero002 = "";
-        int propinas_Mesero002 = 0;
-        int rating_Mesero002 = 0;
-
-//Mesero 2
-        int id_Mesero003 = 10030;
-        String name_Mesero003 = "Rachell Vargas";
-        int mesas_Mesero003 = 0;
-        String turno_Mesero003 = "";
-        int propinas_Mesero003 = 0;
-        int rating_Mesero003 = 0;
-
-//Mesero 2
-        int id_Mesero004 = 10035;
-        String name_Mesero004 = "Maria Mata";
-        int mesas_Mesero004 = 0;
-        String turno_Mesero004 = "";
-        int propinas_Mesero004 = 0;
-        int rating_Mesero004 = 0;
-
-        String datos_Meserostxt = JOptionPane.showInputDialog("Ingrese el numero de mesero");
-        int datos_Meseros = Integer.parseInt(datos_Meserostxt);
-
-        switch (datos_Meseros) {
-            case 10020:
-                JOptionPane.showConfirmDialog(null,
-                        "\nEl ID del mesero es " + id_Mesero001
-                        + "\nEl nombre del mesero es " + name_Mesero001
-                        + "\nLa mesa asignada es: " + mesas_Mesero001
-                        + "\nEl mesero esta en el turno de: " + turno_Mesero001
-                        + "\nLa propina del mesero es " + propinas_Mesero001
-                        + "\nEl rating del mesero es de " + rating_Mesero001);
-                break;
-            case 10025:
-                JOptionPane.showConfirmDialog(null,
-                        "\nEl ID del mesero es " + id_Mesero002
-                        + "\nEl nombre del mesero es " + name_Mesero002
-                        + "\nLa mesa asignada es: " + mesas_Mesero002
-                        + "\nEl mesero esta en el turno de: " + turno_Mesero002
-                        + "\nLa propina del mesero es " + propinas_Mesero002
-                        + "\nEl rating del mesero es de " + rating_Mesero002);
-                break;
-            case 10030:
-                JOptionPane.showConfirmDialog(null,
-                        "El ID del mesero es " + id_Mesero003
-                        + "\nEl nombre del mesero es " + name_Mesero003
-                        + "\nLa mesa asignada es: " + mesas_Mesero003
-                        + "\nEl mesero esta en el turno de: " + turno_Mesero003
-                        + "\nLa propina del mesero es " + propinas_Mesero003
-                        + "\nEl rating del mesero es de " + rating_Mesero003);
-                break;
-            case 10035:
-                JOptionPane.showConfirmDialog(null,
-                        "El ID del mesero es " + id_Mesero004
-                        + "\nEl nombre del mesero es " + name_Mesero004
-                        + "\nLa mesa asignada es: " + mesas_Mesero004
-                        + "\nEl mesero esta en el turno de: " + turno_Mesero004
-                        + "\nLa propina del mesero es " + propinas_Mesero004
-                        + "\nEl rating del mesero es de " + rating_Mesero004);
-                break;
-            default:
-                JOptionPane.showMessageDialog(null, "El mesero no se encontro");
-                break;
-
-        }
-   
-              
+    public Mesero(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+        this.rating = 5;
+        this.propinas = 0;
+        
+        
+    }
     
-            }
+    
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setMesas_asignadas(int[] mesas_asignadas) {
+        this.mesas_asignadas = mesas_asignadas;
+    }
+
+    public void setTurno(Turno_Mesero turno) {
+        this.turno = turno;
+    }
+
+    public void setPropinas(double propinas) {
+        this.propinas = propinas;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int[] getMesas_asignadas() {
+        return mesas_asignadas;
+    }
+
+    public Turno_Mesero getTurno() {
+        return turno;
+    }
+
+    public double getPropinas() {
+        return propinas;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+    
+    
+    
+
+    
+    
+
+}
--- src/main/java/elp/proyecto_restaurante/Restaurante.java
+++ src/main/java/elp/proyecto_restaurante/Restaurante.java
@@ -1,13 +1,38 @@
 
 package elp.proyecto_restaurante;
 
+import java.util.Random;
+
 
 public class Restaurante {
     //Att
-    
-    //Metodos
-    public void Inicializar(){
-        //Se inicializa mesas, meseros etc
+    private Mesa mesas[];
+    private int nume_mesas;
+
+    public Restaurante(){
+        nume_mesas = randint();
+        mesas = new Mesa[nume_mesas];
+        
+    }
+
+    public Mesa[] getMesas() {
+        return mesas;
+    }
+
+    public int getNume_mesas() {
+        return nume_mesas;
     }
     
+    
+    
+    
+    
+    
+    
+    
+    
+    public int randint(){
+        Random ran = new Random();
+        return ran.nextInt(8, 13);
+    }
 }
