2970dc14507d08b141d2575606ec50253266c958
vjndas <Santiagovc590@gmail.com>
2024-03-30 12:13:46 -0600

    pequeñisimo avance sobre las mesas

--- src/main/java/elp/proyecto_restaurante/Mesa.java
+++ src/main/java/elp/proyecto_restaurante/Mesa.java
@@ -1,18 +1,37 @@
-
 package elp.proyecto_restaurante;
 
+import java.util.Random;
 
 public class Mesa {
+
     //Att
-    int numero_mesa;
+    int mesas;
+    int numero_mesa = 0;
     int cantidad_sillas;
     boolean estado_uso;
     boolean tiene_vista;
     Mesero mesero;
-    
-    public void Iniciar_mesa(){
+
+    public void Iniciar_mesa() {
+        numero_mesa+=1;
+        mesas = getNumeroMesas(8, 12);
+        cantidad_sillas = getNumeroSillas(2, 6);
         
+        
+        
+
     }
-    
-    
+
+
+    public static int getNumeroMesas(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(min, max + 1);
+    }
+
+    public static int getNumeroSillas(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(min, max + 1);
+    }
+
 }
+
